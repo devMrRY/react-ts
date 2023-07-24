9b8719987a62e9433ba9a961bcc0b5a2cfee4e72
RAHUL YADAV <yadav.rahul2@coxautoinc.com>
2023-07-24 23:13:55 +0530

    prettify code

--- src/app/Sidebar/page.tsx
+++ src/app/Sidebar/page.tsx
@@ -1,22 +1,23 @@
 import { SyntheticEvent } from "react";
 
 type Props = {
-    handleClick: React.Dispatch<React.SetStateAction<number>>;
-    handleSubmit: <T extends SyntheticEvent>(e: T) => void;
-    inputRef: React.Ref<HTMLInputElement>;
+  handleClick: React.Dispatch<React.SetStateAction<number>>;
+  handleSubmit: <T extends SyntheticEvent>(e: T) => void;
+  inputRef: React.Ref<HTMLInputElement>;
 };
 
 const Sidebar: React.FC<Props> = (props) => {
-    return (
-        <div>
-            <button onClick={e => props.handleClick(prev => ++prev)}>
-                click button from Sidebar
-            </button>
-            <br />
-            <input ref={props.inputRef} /><br />
-            <button onClick={props.handleSubmit}>Submit</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <button onClick={(e) => props.handleClick((prev) => ++prev)}>
+        click button from Sidebar
+      </button>
+      <br />
+      <input ref={props.inputRef} />
+      <br />
+      <button onClick={props.handleSubmit}>Submit</button>
+    </div>
+  );
+};
 
 export default Sidebar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 // in next 13 by default all components are server side component so in order to use
 // any window side work need to add "use client";
 "use client";
-import { AppProps } from 'next/app';
-import { useRef, useState } from 'react';
-import Sidebar from './Sidebar/page';
+import { AppProps } from "next/app";
+import { useRef, useState } from "react";
+import Sidebar from "./Sidebar/page";
 
 type Geo = {
   geo: {
     lat: string;
     lng: string;
   };
-}
+};
 
 type Address = Geo & {
   [k: string]: string;
-}
+};
 
 type User = {
   name: string;
@@ -27,15 +27,14 @@ type User = {
     [k: string]: string;
   };
   address: Address;
-}
+};
 
 type Error = {
   message: string;
   status: number;
-}
+};
 
-type Props = {
-}
+type Props = {};
 
 const Home: React.FC<Props> = (props) => {
   const [click, setClicks] = useState<number>(0);
@@ -44,7 +43,9 @@ const Home: React.FC<Props> = (props) => {
 
   const getData = async (): Promise<void> => {
     try {
-      const users: Array<User> = await fetch(`https://jsonplaceholder.typicode.com/users`).then((res: Response) => res.json());
+      const users: Array<User> = await fetch(
+        `https://jsonplaceholder.typicode.com/users`
+      ).then((res: Response) => res.json());
       if (users.length) {
         setData(users);
       }
@@ -53,28 +54,29 @@ const Home: React.FC<Props> = (props) => {
         console.log(error.message);
       }
     }
-  }
+  };
 
   const handleSubmit = (e: React.SyntheticEvent): void => {
-    setClicks(prev => ++prev);
-    if (inputRef.current?.value)
-      inputRef.current.value = "";
-  }
+    setClicks((prev) => ++prev);
+    if (inputRef.current?.value) inputRef.current.value = "";
+  };
 
   const fetchData = (e: React.SyntheticEvent): void => {
     getData();
-  }
+  };
 
   return (
     <div>
-      <h1>
-        Home page
-      </h1>
+      <h1>Home page</h1>
       <p>click: {click}</p>
       <button onClick={fetchData}>fetch Data</button>
-      <Sidebar handleClick={setClicks} handleSubmit={handleSubmit} inputRef={inputRef} />
+      <Sidebar
+        handleClick={setClicks}
+        handleSubmit={handleSubmit}
+        inputRef={inputRef}
+      />
 
-      {data.map(item => (
+      {data.map((item) => (
         <div>
           <p>Name: {item.name}</p>
           <p>Email: {item.email}</p>
@@ -84,7 +86,7 @@ const Home: React.FC<Props> = (props) => {
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default Home;
